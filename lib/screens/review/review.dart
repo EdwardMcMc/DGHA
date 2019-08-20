82c7059405ff68c82d4dcf1a584a2b8570cd467f
Edward McKenzie-McHarg <102103545@student.swin.edu.au>
2019-08-21 09:43:23 +1000

    navigation donw

---
+++ lib/screens/review/review.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return Text(
+     "Review Page"
+   );
+ }
+}
