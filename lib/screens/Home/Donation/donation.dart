5343eed69e86a54ac5ae02f10d4c750e4a853f92
GitSiracusa <7258267@student.swin.edu.au>
2019-08-21 10:47:34 +1000

    scaffold donation page

--- lib/screens/Home/Donation/donation.dart
+++ lib/screens/Home/Donation/donation.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Donation extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
-   return Text(
-     "Donation Page"
+   return Scaffold(
+     appBar: AppBar(title: Text('Donation')),
+     body: Text("Donations Page")
    );
  }
 }
