33e2395ab6db135a551a6699596d80e9db54cca6
Emilio <emiliokyp@gmail.com>
2019-08-21 11:06:58 +1000

    banner image

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -1,16 +1,26 @@
-import 'package:dgha/screens/home/Donation/donation.dart';
+import 'Donation/donation.dart';
+import 'image_banner.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Info')),
-        body: RaisedButton(
+      appBar: AppBar(title: Text('Home')),
+      body: Container(
+        child: Column(children: [
+          ImageBanner(),
+          RaisedButton(
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute<void>(builder: (context) => Donation()));
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(builder: (context) => Donation()),
+              );
             },
-            child: Text('Donate')));
+            child: Text('Donate'),
+          ),
+        ]),
+      ),
+    );
   }
 }
