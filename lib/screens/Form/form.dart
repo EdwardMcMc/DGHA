615626eece337825ff9952b563101c31aef8d3bd
EDWARD MCKENZIE-MCHARG <102103545@student.swin.edu.au>
2019-08-24 15:19:26 +1000

    cleaned up form code, added one more page

--- lib/screens/Form/form.dart
+++ lib/screens/Form/form.dart
@@ -15,14 +15,16 @@ class MyCustomFormState extends State<MyCustomForm> {
   String _membertype;
   PageController controller=PageController();
   bool membertypevalid=false;
-   String _title;
-   String _fname;
-   String _lname;
-   DateTime selectedDate = DateTime.now();
-   DateFormat dateFormat = DateFormat("MMMM d y");
+  String _title;
+  String _fname;
+  String _lname;
+  String _address;
+  DateTime selectedDate = DateTime.now();
+  DateFormat dateFormat = DateFormat("MMMM d y");
   TextEditingController _titlecontroller=TextEditingController();
   TextEditingController _fnamecontroller=TextEditingController();
   TextEditingController _lnamecontroller=TextEditingController();
+  TextEditingController _addresscontroller=TextEditingController();
 
    Future<Null> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -40,198 +42,272 @@ class MyCustomFormState extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
 
-    return
+  return
     Scaffold(
      appBar: AppBar(title: Text('Membership')),
      body: Form(
       key: _formKey,
       child: 
-      PageView(controller: controller,
-    physics:new NeverScrollableScrollPhysics(),
-      children: <Widget>[Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ListTile(
-            title: const Text('Full Member'),
-            leading: Radio(
-              value: "Full",
-              groupValue: _membertype,
-              onChanged: (String value) {
-                setState(() {
-                 _membertype=value; 
-                });
-                membertypevalid=true;
-              },
-            ),
-          ),
-          ListTile(
-            title: const Text('Associate Member'),
-            leading: Radio(
-              value: "Associate",
-              groupValue: _membertype,
-              onChanged: (String value) {
-                setState(() {
-                 _membertype=value; 
-                });
-                membertypevalid=true;
-              },
-            ),
-          ),
-          Padding(padding:const EdgeInsets.all(10.0),
-          child:                 
-          RaisedButton(
-            onPressed: () {                            
-              // Validate returns true if the form is valid, otherwise false.
-              if (membertypevalid) {
-                 controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
-              }
-              else {
-                Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Please Select Membership Type')));
-              }
-            },
-            child: Text('Submit'),
-          ))
-        ],
-      ),
-    Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("Title"),
-           TextFormField(
-             controller: _titlecontroller,
-              validator: (value) {
-              //  if(value==_title)
-              //  {return null;}
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              else if(value.length>10)
-              {
-                return 'Please enter no more than 10 characters';
-              }
-              _title=value;
-              return null;
-            },
-            
-          ),
-          Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: RaisedButton(
-            onPressed: () {
-              if(_formKey.currentState.validate()){
-              controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
-              }
-            },
-            child: Text("Next"),
-          ),  
-          ),
-        Padding(padding:const EdgeInsets.all(10.0),
-          child: RaisedButton(
-      onPressed: (){
-        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
-      },
-      
-      child: Text("Back"),
-    )
-        )
-        ],
-      ),
- Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-
-          Text("FirstName"),
-          TextFormField(
-            controller: _fnamecontroller,
-            validator: (value) {
-              if(value==_fname)
-              {return null;}
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              else if(value.length>40)
-              {
-                return 'Please enter no more than 40 characters';
-              }
-              _fname=value;
-              return null;
-            },
-          ),
-          Text("Last Name"),
-          TextFormField(
-            controller:_lnamecontroller,
-            validator:(value){
-              if(value==_lname) {
-                return null;
-                }
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              else if(value.length>40)
-              {
-                return 'Please enter no more than 40 characters';
-              }
-               _lname=value;
-              return null; 
-          } ,)
-
-        ,Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: RaisedButton(
-            onPressed: () {
-              if(_formKey.currentState.validate()){
-              controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
-              }
-            },
-            child: Text("Next"),
-          ),  
-          ),
-          Padding(padding: EdgeInsets.all(10.0),
-          child:   
-          RaisedButton(
-      onPressed: (){
-        _titlecontroller.text=_title;
-        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
-      },
-      
-      child: Text("Back"),
-    )
-          )]
-      )
-    
-    ,
-     Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        PageView(controller: controller,
+          physics:new NeverScrollableScrollPhysics(),
           children: <Widget>[
-            Text("Date of Birth (optional)"),
-            Text(dateFormat.format(selectedDate)),
-            SizedBox(height: 20.0,),
-            RaisedButton(
-              onPressed: () => _selectDate(context),
-              child: Text('Select date'),
-            ),
-          ],
-        ),
-      ),
-          RaisedButton(
-      onPressed: (){
-        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
-      },
-      
-      child: Text("Back"),
-    )
+//page 1 start
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  title: const Text('Full Member'),
+                  leading: Radio(
+                    value: "Full",
+                    groupValue: _membertype,
+                    onChanged: (String value) {
+                      setState(() {
+                        _membertype=value; 
+                        });
+                      membertypevalid=true;
+                      },
+                    ),
+                  ),
+                ListTile(
+                  title: const Text('Associate Member'),
+                  leading: Radio(
+                    value: "Associate",
+                    groupValue: _membertype,
+                    onChanged: (String value) {
+                      setState(() {
+                        _membertype=value; 
+                        });
+                      membertypevalid=true;
+                      },
+                    ),
+                  ),
+                Padding(
+                  padding:const EdgeInsets.all(10.0),
+                  child:                 
+                    RaisedButton(
+                      onPressed: () {                            
+                      // Validate returns true if the form is valid, otherwise false.
+                        if (membertypevalid) {
+                          controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
+                          }
+                        else {
+                          Scaffold.of(context).showSnackBar(SnackBar(content: Text('Please Select Membership Type')));
+                          }
+                        },
+                      child: Text('Submit'),
+                      )
+                  )
+                ],
+              ),
 
-        ]
-      )
-    ,
-    Text("page6")
-    ,
-    Text("page7")
+//Page 2 Start
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Title"),
+                  TextFormField(
+                    controller: _titlecontroller,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                        }
+                      else if(value.length>10) {
+                        return 'Please enter no more than 10 characters';
+                        }
+                      _title=value;
+                      return null;
+                      }, 
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if(_formKey.currentState.validate()){
+                          controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
+                          }
+                        },
+                      child: Text("Next"),
+                      ),  
+                    ),
+                  Padding(padding:const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                        },
+                      child: Text("Back"),
+                      )
+                    )
+                  ],
+                ),
+
+//page 3 start
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("FirstName"),
+                  TextFormField(
+                    controller: _fnamecontroller,
+                    validator: (value) {
+                      if(value==_fname)
+                      {return null;}
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      else if(value.length>40)
+                      {
+                        return 'Please enter no more than 40 characters';
+                      }
+                      _fname=value;
+                      return null;
+                      },
+                    ),
+                  Text("Last Name"),
+                  TextFormField(
+                    controller:_lnamecontroller,
+                    validator:(value){
+                      if(value==_lname) {
+                        return null;
+                        }
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                        }
+                      else if(value.length>40) {
+                        return 'Please enter no more than 40 characters';
+                        }
+                      _lname=value;
+                      return null; 
+                      },
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if(_formKey.currentState.validate()){
+                          controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
+                          }
+                        },
+                      child: Text("Next"),
+                      ),  
+                    ),
+                  Padding(padding: EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        _titlecontroller.text=_title;
+                        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                        },
+                      child: Text("Back"),
+                      )
+                    )
+                  ]
+                ),
+
+//page 4 start
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text("Date of Birth (optional)"),
+                        Text(dateFormat.format(selectedDate)),
+                        RaisedButton(
+                          onPressed: () => _selectDate(context),
+                          child: Text('Select date'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        if (_formKey.currentState.validate()) {
+                          controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
+                          }
+                      },
+                      child: Text("Next"),
+                    ),
+                    ),
+                    Padding(
+                    padding: const EdgeInsets.all(10.0),  
+                    child: RaisedButton(
+                      onPressed: (){
+                        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                        },
+                      child: Text("Back"),
+                      )
+                    ),
+                  ]
+                ),
+
+    //Page 5 Start
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Stret or Postal Address"),
+                  TextFormField(
+                    controller: _addresscontroller,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                        }
+                      else if (value.length>70) {
+                        return 'Please enter no more than 70 characters';
+                        }
+                      _address=value;
+                      return null;
+                      },  
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if(_formKey.currentState.validate()) {
+                          controller.nextPage(duration: kTabScrollDuration,curve: Curves.ease);
+                          }
+                        },
+                      child: Text("Next"),
+                      ),  
+                    ),
+                  Padding(padding:const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                        },
+                      child: Text("Back"),
+                      )
+                    ),
+                  // Padding(padding:const EdgeInsets.all(10.0),
+                  //   child: RaisedButton(
+                  //     onPressed: (){
+                  //       controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                  //       },     
+                  //     child: Text("Back"),
+                  //     )
+                  //   )
+                  ],
+                ),
+
+    //page 6 Start
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("page7"),
+                  Padding(padding:const EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        controller.previousPage(duration: kTabScrollDuration,curve: Curves.ease);
+                        },
+                      child: Text("Back"),
+                      )
+                    ),
+                  ]
+                )
     ,
     Text("page8")
     ,
