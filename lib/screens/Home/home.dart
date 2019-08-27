f504f872f68ae2e9ff2b023af59ac2ef50bacfcc
James Glover <james@sparkeleven.com>
2019-08-27 11:28:31 +1000

    Added home content

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -8,38 +8,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Container(
-        child: Column(children: [
-          ImageBanner(),
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(builder: (context) => MyCustomForm()),
-              );
-            },
-            child: Text('Membership'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (context) => WebviewScaffold(
-                    url:
-                        "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@gmg.cm&lc=AU&item_name=Donations+or+Membership+Fee+or+Conference+Registration&no_note=0&cn=&currency_code=AUD&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted",
-                    appBar: new AppBar(
-                      title: new Text("Donate"),
+        appBar: AppBar(title: Text('Home')),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(children: [
+              ImageBanner(),
+              Text(
+                "About Dog Guide Handlers Australia",
+                style: Theme.of(context).textTheme.title,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Dog Guide Handlers Australia (DGHA) is an independent non profit association of dog guide handlers, and is not affiliated with any dog guide organisation.',
+                style: Theme.of(context).textTheme.body1,
+                textAlign: TextAlign.center,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                        builder: (context) => MyCustomForm()),
+                  );
+                },
+                child: Text('Membership'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (context) => WebviewScaffold(
+                        url:
+                            "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@gmg.cm&lc=AU&item_name=Donations+or+Membership+Fee+or+Conference+Registration&no_note=0&cn=&currency_code=AUD&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted",
+                        appBar: new AppBar(
+                          title: new Text("Donate"),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
-            child: Text('Donate'),
+                  );
+                },
+                child: Text('Donate'),
+              ),
+              Text(
+                "Key Objectives",
+                style: Theme.of(context).textTheme.title,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'DGHA draws on the experiences and varied knowledge of its members, to provide peer support, advocacy and information for dog guide handlers throughout Australia.',
+                style: Theme.of(context).textTheme.body1,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Background Information",
+                style: Theme.of(context).textTheme.title,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Dog Guide Handlers Australia was formed in August 2009 by four passionate people who were inspired by similar groups which exist in other countries.\n\n'
+                'DGHA was registered as an incorporated association in January 2011.\n'
+                'DGHA does not have a physical office, and we have no paid staff.\n'
+                'The DGHA Committee is a group of dedicated volunteers who meet regularly via teleconference.',
+                style: Theme.of(context).textTheme.body1,
+                textAlign: TextAlign.center,
+              ),
+            ]),
           ),
-        ]),
-      ),
-    );
+        ));
   }
 }
