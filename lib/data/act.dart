6ca11945fa10fb7c8d835e6855986692af200fb6
Nathan <7258267@student.swin.edu.au>
2019-09-03 09:42:15 +1000

    spacing fix

--- lib/data/act.dart
+++ lib/data/act.dart
@@ -16,7 +16,7 @@ class ACT extends State {
         title: 'Domestic Animals Act 2000',
         link:
             'http://www.legislation.act.gov.au/a/2000-86/current/pdf/2000-86.pdf',
-        section: '\r\rSection/s Applicable:\r\r Section 104, Rights of Persons Accompanied by Assistance Animals\r\rSection 105, Exclusion of Assistance Animal from Public Place\r\rSection 106, Imposition of Excess Charges for Assistance Animal\r\r',
+        section: '\n\nSection/s Applicable:\n\n Section 104, Rights of Persons Accompanied by Assistance Animals\n\nSection 105, Exclusion of Assistance Animal from Public Place\n\nSection 106, Imposition of Excess Charges for Assistance Animal\n\n',
         body: [
           'Excerpt from Section 104:',
           '(1) Despite any other territory law (other than the Discrimination Act 1991 ), a person with a disability',
