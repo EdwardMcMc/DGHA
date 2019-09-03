3fdd4efb4f33b00bc6c0f0ef7d511428db03afec
James Glover <james@sparkeleven.com>
2019-09-03 12:01:08 +1000

    Changed Vic Legislation

--- lib/data/vic.dart
+++ lib/data/vic.dart
@@ -16,18 +16,18 @@ class VIC extends State {
         title: 'Domestic Animals ACT 1994',
         link:
             'http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/',
-        section: '\n\nSection/s Applicable:\n\nSection 7, Exemptions for Guide Dogs\n\n',
+        section: '\n\nSection/s Applicable:\n\nSection 7, Exemptions for Guide Dogs\n',
         body: [
-          '(1) A visually impaired person or hearing impaired person who keeps and uses a dog as a guide dog is not',
-          'liable for an offence under this Act in respect of that dog, where, in acting in the manner which would',
+          '\n(1) A visually impaired person or hearing impaired person who keeps and uses a dog as a guide dog is not'+
+          'liable for an offence under this Act in respect of that dog, where, in acting in the manner which would'+
           'constitute the offence, the dog was performing its functions as a guide dog.\n',
-          '(4) Despite anything in any other Act or subordinate instrument a visually impaired person, hearing',
-          'impaired person or person fftraining a guide dog may, at all times and in all places, be accompanied by a',
+          '(4) Despite anything in any other Act or subordinate instrument a visually impaired person, hearing'+
+          'impaired person or person fftraining a guide dog may, at all times and in all places, be accompanied by a'+
           'dog kept and used, or trained by him or her as a guide dog.',
           '',
-          'Maximum Fine for Breach of Act:',
-          'Value of One Penalty Unit as of 2012:',
-          '\$125.19',
+          '\nMaximum Fine for Breach of Act:',
+          'Value of One Penalty Unit as of 2012: \$125.19',
+          '',
         ],
       ),
     );
