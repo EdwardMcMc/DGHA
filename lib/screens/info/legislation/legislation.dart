293ae76769fe2f487b66c2c9726416daeee2df9d
James Glover <james@sparkeleven.com>
2019-09-03 14:30:39 +1000

    Fixed Geolocation on Android

--- lib/screens/info/legislation/legislation.dart
+++ lib/screens/info/legislation/legislation.dart
@@ -1,3 +1,4 @@
+import 'package:dgha/data/act.dart';
 import 'package:flutter/material.dart';
 import 'package:dgha/data/nsw.dart';
 import 'package:dgha/data/vic.dart';
@@ -16,8 +17,53 @@ class Legislation extends StatelessWidget {
   List<Widget> _renderLegislation(context) {
     dynamic state;
     switch (this._state) {
-      case 'VIC':
+      case ('VIC'):
+        state = VIC();
+        break;
+      case 'NSW':
+        state = NSW();
+        break;
+      case 'QLD':
         state = QLD();
+        break;
+      case 'NT':
+        state = NT();
+        break;
+      case 'SA':
+        state = SA();
+        break;
+      case 'TAS':
+        state = TAS();
+        break;
+      case 'WA':
+        state = WA();
+        break;
+      case 'ACT':
+        state = ACT();
+        break;
+      case 'Victoria':
+        state = VIC();
+        break;
+      case 'New South Wales':
+        state = NSW();
+        break;
+      case 'Queensland':
+        state = QLD();
+        break;
+      case 'Northern Territory':
+        state = NT();
+        break;
+      case 'South Australia':
+        state = SA();
+        break;
+      case 'Tasmania':
+        state = TAS();
+        break;
+      case 'Western Australia':
+        state = WA();
+        break;
+      case 'Australian Capital Territory':
+        state = ACT();
         break;
       default:
         state = VIC();
