9d09b7ecdf1d97afff00cebdea9bd2250183abfd
James Glover <james@sparkeleven.com>
2019-09-10 12:37:19 +1000

    Access Page Markdown

---
+++ lib/data/markdown/access.dart
@@ -0,0 +1,49 @@
+const String access = """# Legal obligations
+
+In line with Victorian State Laws, a person with low vision accompanied by a Guide Dog is permitted to:
+
+- Travel on any form of public transport, including taxis, buses, trams and trains
+- Enter any public place
+- Visit any theatre
+- Eat in any restaurant
+- Shop in any store (including supermarkets and food stores)
+
+The only exceptions where a Guide Dog is not permitted are operating theatres, commercial kitchens and some parts of the zoo.
+
+It is generally discriminatory and unlawful for anyone to attempt to deny these rights to a person with low vision, or treat that person less favourably, solely because he or she is accompanied by a Guide Dog.
+
+These laws also apply to people training Guide Dogs and Guide Dog puppies.
+""";
+
+class Access {
+ String english = """# Legal obligations
+
+In line with Victorian State Laws, a person with low vision accompanied by a Guide Dog is permitted to:
+
+- Travel on any form of public transport, including taxis, buses, trams and trains
+- Enter any public place
+- Visit any theatre
+- Eat in any restaurant
+- Shop in any store (including supermarkets and food stores)
+
+The only exceptions where a Guide Dog is not permitted are operating theatres, commercial kitchens and some parts of the zoo.
+
+It is generally discriminatory and unlawful for anyone to attempt to deny these rights to a person with low vision, or treat that person less favourably, solely because he or she is accompanied by a Guide Dog.
+
+These laws also apply to people training Guide Dogs and Guide Dog puppies.
+""";
+
+ String arabic = """""";
+ String chineses = """""";
+ String chineset = """""";
+ String german = """""";
+ String greek = """""";
+ String hindi = """""";
+ String italian = """""";
+ String korean = """""";
+ String nepali = """""";
+ String persian = """""";
+ String punjabi = """""";
+ String spanish = """""";
+ String filipino = """""";
+ String vietnamese = """""";
