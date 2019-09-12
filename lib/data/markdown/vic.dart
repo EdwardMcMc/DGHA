458e6b312eec3573ea914876acc5a8c8edcbe260
Nathan <7258267@student.swin.edu.au>
2019-09-12 11:45:46 +1000

    vic translation

--- lib/data/markdown/vic.dart
+++ lib/data/markdown/vic.dart
@@ -1,31 +1,184 @@
 class VIC {
   List<String> translations = [
-    """# Domestic Animals ACT 1994
- ## Section/s Applicable:
- ### Section 7, Exemptions for Guide Dogs
- [http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+"""# Domestic Animals ACT 1994
+## Section/s Applicable:
+### Section 7, Exemptions for Guide Dogs
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
 
- A visually impaired person or hearing impaired person who keeps and uses a dog as a guide dog is not liable for an offence under this Act in respect of that dog, where, in acting in the manner  which would constitute the offence, the dog was performing its functions as a guide dog.
+A visually impaired person or hearing impaired person who keeps and uses a dog as a guide dog is not liable for an offence under this Act in respect of that dog, where, in acting in the manner  which would constitute the offence, the dog was performing its functions as a guide dog.
 
- (4) Despite anything in any other Act or subordinate instrument a visually impaired person, hearing impaired person or person fftraining a guide dog may, at all times and in all places, be accompanied by a dog kept and used, or trained by him or her as a guide dog.
+(4) Despite anything in any other Act or subordinate instrument a visually impaired person, hearing impaired person or person fftraining a guide dog may, at all times and in all places, be accompanied by a dog kept and used, or trained by him or her as a guide dog.
 
- Maximum Fine for Breach of Act:
- Value of One Penalty Unit as of 2012: \$125.19
- """,
-    "vic Arabic example",
-    "vic 3rd language",
-    "vic 4th language",
-    "vic 5th language",
-    "vic 6th language",
-    "vic 7th language",
-    "vic 8th language",
-    "vic 9th language",
-    "vic 10th language",
-    "vic 11th language",
-    "vic 12th language",
-    "vic 13th language",
-    "vic 14th language",
-    "vic 15th language",
+Maximum Fine for Breach of Act:
+Value of One Penalty Unit as of 2012: \$125.19
+""",
+"""# الحيوانات الأليفة ACT 1994
+## القسم / ثانية قابلة للتطبيق:
+### القسم 7 ، إعفاءات الكلاب المرشدة
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+لا يتحمل أي شخص ضعيف البصر أو شخص ضعيف السمع يحتفظ بالكلب ويستخدمه كدليل ، أي جرم بموجب هذا القانون فيما يتعلق بذلك الكلب ، حيث كان الكلب ، في التصرف بالطريقة التي تشكل الجرم ، يؤدي وظائفها بمثابة كلب دليل.
+
+(4) على الرغم من أي شيء في أي قانون أو صك ثانوي ، فإن أي شخص ضعيف البصر أو شخص ضعيف السمع أو شخص يتدرب على كلب مرشد ، في جميع الأوقات وفي جميع الأماكن ، قد يكون مصحوبًا بكلب يحتفظ به ويستخدمه أو يدربه أو لها كدليل الكلب.
+
+الغرامة القصوى لخرق الفعل:
+قيمة وحدة الجزاء الواحدة اعتبارا من عام 2012: 125.19 دولار
+""",
+"""＃家畜ACT 1994
+##部分适用：
+###第7节，导盲犬豁免
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/）
+
+视障人士或听障人士保留并使用狗作为导盲犬，对于该犬的违法行为不承担责任，如果该犬以构成犯罪的方式行事，则该狗正在表演 它作为导盲犬的功能。
+
+（4）除了任何其他法令或附属文书中的任何内容外，视障人士，听力受损人士或引导犬的人均可随时及在任何地方陪同，饲养或使用或由他训练的狗只或 她作为导盲犬。
+
+违反法案的最高罚款：
+截至2012年一个惩罚单位的价值：125.19美元
+""",
+"""＃家畜ACT 1994
+##部分適用：
+###第7節，導盲犬豁免
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/）
+
+視障人士或聽障人士保留並使用狗作為導盲犬，對於該犬的違法行為不承擔責任，如果該犬以構成犯罪的方式行事，則該狗正在表演 它作為導盲犬的功能。
+
+（4）除了任何其他法令或附屬文書中的任何內容外，視障人士，聽力受損人士或引導犬的人均可隨時及在任何地方陪同，飼養或使用或由他訓練的狗隻或 她作為導盲犬。
+
+違反法案的最高罰款：
+截至2012年一個懲罰單位的價值：125.19美元
+""",
+"""# Lokal na Mga Hayop ACT 1994
+# # Seksyon / s Naaangkop:
+### Seksyon 7, Mga Eksplikasyon para sa Mga Aso sa Gabay
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/sep(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+Ang isang taong may kapansanan sa paningin o taong may kapansanan sa pandinig na nagpapanatili at gumagamit ng isang aso bilang isang gabay na aso ay hindi mananagot para sa isang pagkakasala sa ilalim ng Batas na ito tungkol sa aso na iyon, kung saan, sa pag-arte sa paraang bumubuo sa pagkakasala, ang aso ay gumaganap ang mga function nito bilang isang dog dog.
+
+(4) Sa kabila ng anumang bagay sa anumang iba pang Batas o subordinate na instrumento ng isang taong may kapansanan sa paningin, may kapansanan sa pandinig o taong may kapansanan sa isang gabay na aso ay maaaring, sa lahat ng oras at sa lahat ng mga lugar, ay sinamahan ng isang aso na pinanatili at ginamit, o sinanay sa kanya o siya bilang isang aso na gabay.
+
+Pinakamataas na Fine para sa paglabag sa Batas:
+Halaga ng Isang Unit ng Parusa noong 2012: \$ 125.19
+""",
+"""# Haustiere ACT 1994
+## Section / s Anwendbar:
+### Abschnitt 7, Ausnahmen für Blindenhunde
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)(http://www.austlii.edu.au/legis/vic/consol_act/daa1994163/]
+
+Eine sehbehinderte Person oder eine hörbehinderte Person, die einen Hund als Blindenhund unterhält und verwendet, haftet nicht für eine Straftat nach diesem Gesetz in Bezug auf diesen Hund, wenn der Hund in der Weise handelt, die die Straftat darstellen würde seine Funktionen als Blindenhund.
+
+(4) Eine sehbehinderte Person, eine hörbehinderte Person oder eine Person, die einen Blindenhund ausbildet, kann trotz aller Bestimmungen in anderen Gesetzen oder nachgeordneten Instrumenten jederzeit und an jedem Ort von einem Hund begleitet werden, der von ihm gehalten und benutzt oder ausgebildet wird, oder sie als Blindenhund.
+
+Höchststrafe für Verstöße:
+Wert einer Strafeinheit ab 2012: 125,19 USD
+""",
+"""# Domestic Animals ACT 1994
+## Τμήμα / s Εφαρμοστέο:
+### Τμήμα 7, Εξαιρέσεις για σκυλιά οδηγών
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+Ένα άτομο με προβλήματα όρασης ή άτομο με προβλήματα ακοής που φυλάσσει και χρησιμοποιεί ένα σκύλο ως οδηγό σκυλί δεν είναι υπεύθυνο για αδίκημα σύμφωνα με τον παρόντα νόμο για τον εν λόγω σκύλο, όπου ο σκύλος, ενεργώντας κατά τον τρόπο που θα συνιστούσε το αδίκημα τις λειτουργίες του ως οδηγού σκύλου.
+
+(4) Παρά οτιδήποτε άλλο νόμο ή υποκείμενο μέσο, ένα άτομο με προβλήματα όρασης, άτομο με μειωμένη όραση ή άτομο που υποβαθμίζει έναν σκύλο οδηγό μπορεί ανά πάσα στιγμή και σε όλα τα μέρη να συνοδεύεται από ένα σκύλο που τηρείται και χρησιμοποιείται ή εκπαιδεύεται από αυτόν ή της ως σκύλου-οδηγός.
+
+Μέγιστο πρόστιμο για παραβίαση νόμου:
+Αξία μιας ποινικής μονάδας από το 2012: \$ 125,19
+""",
+"""# घरेलू पशु अधिनियम 1994
+## धारा / एस लागू:
+### धारा 7, गाइड कुत्तों के लिए छूट
+[Http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+एक दृष्टिहीन व्यक्ति या श्रवण बाधित व्यक्ति, जो एक कुत्ते को एक गाइड कुत्ते के रूप में रखता है और उसका उपयोग करता है, उस कुत्ते के संबंध में इस अधिनियम के तहत अपराध के लिए उत्तरदायी नहीं है, जहां, उस तरीके से अभिनय में, जो अपराध का गठन करेगा, कुत्ता प्रदर्शन कर रहा था। एक गाइड कुत्ते के रूप में इसके कार्य।
+
+(४) किसी भी अन्य अधिनियम या अधीनस्थ साधन में कुछ भी होने के बावजूद एक दृष्टिबाधित व्यक्ति, श्रवण बाधित व्यक्ति या व्यक्ति एक गाइड कुत्ते को हर समय और सभी स्थानों पर, एक कुत्ते को रखा और इस्तेमाल किया जा सकता है, या उसके द्वारा प्रशिक्षित या उसके साथ प्रशिक्षित किया जा सकता है। एक गाइड कुत्ते के रूप में।
+
+अधिनियम के उल्लंघन के लिए अधिकतम जुर्माना:
+2012 के रूप में एक दंड इकाई का मूल्य: \$ 125.19
+""",
+"""# Domestic Animals ACT 1994
+## Sezione / i applicabile:
+### Sezione 7, Esenzioni per i cani guida
+[Http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+Una persona ipovedente o non udente che detiene e utilizza un cane come cane guida non è responsabile per un reato ai sensi della presente legge nei confronti di quel cane, dove, nel comportarsi nel modo che costituirebbe il reato, il cane stava eseguendo funziona come un cane guida.
+
+(4) Nonostante qualsiasi cosa in qualsiasi altro atto o strumento subordinato, una persona ipovedente, una persona con problemi di udito o una persona che segue un cane guida può essere sempre accompagnata da un cane tenuto e usato, o addestrato da lui o lei come cane guida.
+
+Multa massima per violazione dell'atto:
+Valore di un'unità di penalità a partire dal 2012: \$ 125,19
+""",
+"""# 국내 동물 ACT 1994
+적용 가능한 섹션 :
+### 섹션 7, 안내견 면제
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/](http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+개를 안내견으로 유지하고 사용하는 시각 장애인 또는 청각 장애인은 해당 개와 관련하여 본 법에 따른 위반에 대해 책임을지지 않습니다. 안내견으로서의 기능.
+
+(4) 다른 법률 또는 종속기구의 어떤 내용에도 불구하고 시각 장애인, 청각 장애인 또는 안내견을 훈련시키는 사람은 항상 그리고 모든 장소에서 개가 동반하여 사용하거나 훈련을받을 수 있습니다. 그녀를 안내견으로
+
+법 위반에 대한 최대 벌금 :
+2012 년 기준 한 페널티 유닛의 가치 : \$ 125.19
+""",
+"""# घरेलु पशु एक्ट १ 199 ACT 199
+## सेक्सन / हरू लागू:
+### सेक्सन,, गाईड कुकुरहरूका लागि छुटहरू
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/ छुट(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+गाइड कुकुरको रूपमा कुकुर राख्ने र प्रयोग गर्ने दृष्टिविहीन व्यक्ति वा सुन्न नसक्ने व्यक्तिको कुकुरको सन्दर्भमा यस ऐन अन्तर्गत कसुरको लागि उत्तरदायी हुँदैन, जहाँ अपराधको रूपमा काम गर्ने कुकुरले प्रदर्शन गरिरहेको थियो। गाइड कुकुरको रूपमा यसको कार्यहरू।
+
+()) कुनै अन्य ऐन वा अधीनस्थ उपकरणमा केही चीजको बाबजुद पनि दृष्टिविहीन व्यक्ति, सुन्न नसक्ने व्यक्ति वा गाईड कुकुरको उपचार गर्ने व्यक्ति, जहिले पनि र सबै ठाउँमा, कुकुर संगै राखिएको र प्रयोग हुन सक्छ, वा उहाँद्वारा प्रशिक्षित वा उनी गाईड कुकुरको रूपमा।
+
+कानूनको उल्लंघनको लागि अधिकतम फाइनः
+२०१२ सम्म एक पेनाल्टी एकाईको मान: \$ १२.१ .1
+""",
+"""# حیوانات خانگی ACT 1994
+## بخش / ها قابل اجرا:
+### بخش 7 ، معافیت سگهای راهنما
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/ თარგმ(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+شخص دارای اختلال بینایی یا شنوایی که سگ را به عنوان سگ راهنما نگه می دارد و از آن استفاده می کند ، نسبت به آن سگ هیچ گونه مسئولیتی در قبال این قانون ندارد ، جایی که با عمل به روشی که جرم را تشکیل می دهد ، سگ انجام می داد. عملکرد آن به عنوان یک سگ راهنما است.
+
+(4) علی رغم هر آنچه در قانون یا ابزار فرعی دیگری وجود دارد ، فرد دارای اختلال در بینائی ، فرد دارای اختلال شنوایی یا شخصی که از سگ راهنما سرقت می کند ممکن است در همه زمانها و در همه مکانها با یک سگ نگهداری شده و مورد استفاده قرار گیرد یا توسط او آموزش دیده یا او به عنوان یک سگ راهنما
+
+حداکثر جریمه برای نقض قانون:
+ارزش واحد پنالتی از سال 2012: 125.19 دلار
+""",
+"""# ਘਰੇਲੂ ਜਾਨਵਰ ਐਕਟ 1994
+## ਭਾਗ / ਸ ਲਾਗੂ
+### ਸੈਕਸ਼ਨ 7, ਗਾਈਡ ਕੁੱਤਿਆਂ ਲਈ ਛੋਟਾਂ
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/ ਅਦਾਲਤ(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+ਇਕ ਦ੍ਰਿਸ਼ਟੀਹੀਣ ਵਿਅਕਤੀ ਜਾਂ ਸੁਣਨ ਵਾਲਾ ਕਮਜ਼ੋਰ ਵਿਅਕਤੀ ਜੋ ਇਕ ਕੁੱਤੇ ਨੂੰ ਗਾਈਡ ਕੁੱਤੇ ਵਜੋਂ ਰੱਖਦਾ ਹੈ ਅਤੇ ਵਰਤਦਾ ਹੈ, ਉਸ ਕੁੱਤੇ ਦੇ ਸੰਬੰਧ ਵਿਚ ਇਸ ਐਕਟ ਅਧੀਨ ਕਿਸੇ ਜੁਰਮ ਲਈ ਜ਼ਿੰਮੇਵਾਰ ਨਹੀਂ ਹੈ, ਜਿਥੇ, ਇਸ offੰਗ ਨਾਲ ਕੰਮ ਕਰਨਾ ਜੋ ਅਪਰਾਧ ਨੂੰ ਦਰਸਾਉਂਦਾ ਹੈ, ਕੁੱਤਾ ਪ੍ਰਦਰਸ਼ਨ ਕਰ ਰਿਹਾ ਸੀ ਇੱਕ ਗਾਈਡ ਕੁੱਤੇ ਦੇ ਤੌਰ ਤੇ ਇਸ ਦੇ ਕੰਮ.
+
+()) ਕਿਸੇ ਹੋਰ ਐਕਟ ਜਾਂ ਅਧੀਨ ਯੰਤਰ ਵਿੱਚ ਕਿਸੇ ਦ੍ਰਿਸ਼ਟੀਹੀਣ ਵਿਅਕਤੀ ਦੇ ਬਾਵਜੂਦ, ਸੁਣਿਆ ਹੋਇਆ ਕਮਜ਼ੋਰ ਵਿਅਕਤੀ ਜਾਂ ਇੱਕ ਗਾਈਡ ਕੁੱਤੇ ਨਾਲ ਜੁੜੇ ਵਿਅਕਤੀ ਨੂੰ, ਹਰ ਸਮੇਂ ਅਤੇ ਸਾਰੀਆਂ ਥਾਵਾਂ ਤੇ, ਇੱਕ ਕੁੱਤੇ ਦੇ ਨਾਲ ਰੱਖਿਆ ਜਾ ਸਕਦਾ ਹੈ ਅਤੇ ਵਰਤਿਆ ਜਾ ਸਕਦਾ ਹੈ, ਜਾਂ ਉਸ ਦੁਆਰਾ ਸਿਖਲਾਈ ਦਿੱਤੀ ਜਾ ਸਕਦੀ ਹੈ ਜਾਂ ਉਸ ਨੂੰ ਇਕ ਗਾਈਡ ਕੁੱਤੇ ਵਜੋਂ.
+
+ਕਾਨੂੰਨ ਦੀ ਉਲੰਘਣਾ ਲਈ ਵੱਧ ਤੋਂ ਵੱਧ ਜੁਰਮਾਨਾ:
+2012 ਤਕ ਇਕ ਪੈਨਲਟੀ ਯੂਨਿਟ ਦੀ ਕੀਮਤ: .1 125.19
+""",
+"""# Ley de animales domésticos de 1994
+## Sección / s aplicable:
+### Sección 7, Exenciones para perros guía
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/font>(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+Una persona con discapacidad visual o con discapacidad auditiva que tiene y utiliza un perro como perro guía no es responsable de un delito en virtud de esta Ley con respecto a ese perro, donde, al actuar de la manera que constituiría el delito, el perro estaba realizando Funciona como un perro guía.
+
+(4) A pesar de cualquier cosa en cualquier otra Ley o instrumento subordinado, una persona con discapacidad visual, una persona con discapacidad auditiva o una persona que entrena a un perro guía puede, en todo momento y en todos los lugares, estar acompañado por un perro mantenido y utilizado, o entrenado por él o ella como perro guía.
+
+Multa máxima por incumplimiento de la ley:
+Valor de una unidad de penalización a partir de 2012: \$ 125.19
+""",
+"""# Động vật nuôi ACT 1994
+## Phần / s Áp dụng:
+### Phần 7, Miễn cho Chó dẫn đường
+[http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/[(http://www.austlii.edu.au/au/legis/vic/consol_act/daa1994163/)
+
+Người khiếm thị hoặc người khiếm thính giữ và sử dụng chó làm chó dẫn đường không chịu trách nhiệm hình sự theo Đạo luật này đối với con chó đó, trong đó, hành động theo cách cấu thành tội phạm, con chó đang thực hiện chức năng của nó như một con chó dẫn đường.
+
+(4) Mặc dù có bất kỳ điều gì trong bất kỳ Đạo luật hoặc công cụ cấp dưới nào khác, người khiếm thị, người khiếm thính hoặc người đang theo dõi một con chó dẫn đường có thể, mọi lúc và mọi nơi, được đi kèm với một con chó được giữ và sử dụng, hoặc được huấn luyện bởi anh ta hoặc cô như một con chó dẫn đường.
+
+Mức phạt tối đa cho Vi phạm Đạo luật:
+Giá trị của một đơn vị phạt vào năm 2012: \$ 125,19
+""",
   ];
-
 }
