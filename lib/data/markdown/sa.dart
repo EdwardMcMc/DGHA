62b3d2ee0fea07426b78b81b9ec20ec2d0d091a7
Nathan <7258267@student.swin.edu.au>
2019-09-12 11:21:24 +1000

    sa translation

--- lib/data/markdown/sa.dart
+++ lib/data/markdown/sa.dart
@@ -1,4 +1,5 @@
-const String SA = """# Dog and Cat Management Act 1995
+class QLD {
+  List<String> translations = ["""# Dog and Cat Management Act 1995
 ## Section/s Applicable:
 ### Section 21, Accreditation of Disability Dogs, Guide Dogs etc
 ### Section 81, Disability Dogs, Guide Dogs etc
@@ -14,4 +15,242 @@ Fixed penalty of \$250
 
 Exceptions to Access Under Act:
 It is not unlawful for a person with an assistance animal to be asked to produce evidence that their dog is an assistance animal.
-""";
+""",
+"""# قانون إدارة الكلاب والقطط 1995
+## القسم / ثانية قابلة للتطبيق:
+### القسم 21 ، اعتماد كلاب الإعاقة ، دليل الكلاب ، إلخ
+### القسم 81 ، كلاب الإعاقة ، دليل الكلاب ، إلخ
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog٪20and٪20Cat٪20Management٪20Act٪201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog٪20and٪20Cat٪20Management٪20Act٪201995.aspx/)
+
+مقتطف من القسم 81:
+(1) على الرغم من هذا القانون أو أي قانون آخر؟
+(أ) الشخص المكفوف أو الأصم كليًا أو جزئيًا ، أو المعاق بطريقة أخرى ، يحق له أن يكون مصحوبًا بكلاب معاق معاق أو كلب مرشد أو كلب سمع في مكان عام أو سيارة ركاب عامة ؛ و
+(ب) يجب على المحتل أو الشخص المسؤول عن مكان عام أو مركبة نقل الركاب العامة ألا يرفض الوصول إلى المكان أو السيارة إلى شخص مصاب بالعمى أو الصمم كليًا أو جزئيًا ، أو معاق بطريقة أخرى ، على الأرض التي يكون الشخص مصحوبًا بها بواسطة كلب إعاقة أو كلب مرشد أو كلب سماع.
+
+الغرامة القصوى لخرق الفعل:
+عقوبة ثابتة قدرها 250 دولار
+
+استثناءات الوصول بموجب القانون:
+ليس من غير القانوني أن يطلب من شخص لديه حيوان مساعدة تقديم دليل على أن كلبه هو حيوان مساعد.
+""",
+"""＃1995年“狗与猫管理法”
+##部分适用：
+###第21节，残疾犬，导盲犬等的认证
+###第81节，残疾犬，导盲犬等
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/）
+
+摘自第81节：
+（1）尽管该法案或任何其他法案？
+（a）全部或部分失明或失聪或以其他方式残疾的人，有权在公众地方或公共乘客车辆上由认可的残疾狗，导盲犬或助听犬陪同;和
+（b）公众地方或公共乘客车辆的占用人或负责人不得以该人陪同为由拒绝全部或部分失明或失聪或以其他方式残疾的人进入该地方或车辆由残疾狗，导盲犬或助听犬。
+
+违反法案的最高罚款：
+定额罚款250美元
+
+法案下访问的例外情况：
+援助动物的人被要求出示其狗是辅助动物的证据并非违法。
+""",
+"""＃1995年“狗與貓管理法”
+##部分適用：
+###第21節，殘疾犬，導盲犬等的認證
+###第81節，殘疾犬，導盲犬等
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/）
+
+摘自第81節：
+（1）儘管該法案或任何其他法案？
+（a）全部或部分失明或失聰或以其他方式殘疾的人，有權在公眾地方或公共乘客車輛上由認可的殘疾狗，導盲犬或助聽犬陪同;和
+（b）公眾地方或公共乘客車輛的佔用人或負責人不得以該人陪同為由拒絕全部或部分失明或失聰或以其他方式殘疾的人進入該地方或車輛由殘疾狗，導盲犬或助聽犬。
+
+違反法案的最高罰款：
+定額罰款250美元
+
+法案下訪問的例外情況：
+援助動物的人被要求出示其狗是輔助動物的證據並非違法。
+""",
+"""# Pamamahala sa Aso at Cat 1995
+# # Seksyon / s Naaangkop:
+### Seksyon 21, Accreditation of Disability Dogs, Gabay sa Aso atbp
+### Seksyon 81, Mga Kapansanan sa Aso, Gabay sa Aso atbp
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/ihara(http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+Sipi mula sa Seksyon 81:
+(1) Sa kabila ng Batas na ito o anumang iba pang Batas?
+(a) ang isang tao na buo o bahagyang bulag o bingi, o kung hindi man ay may kapansanan, ay may karapatan na samahan ng isang aso na akreditadong may kapansanan, gabay ng aso o pandinig na aso sa isang pampublikong lugar o pampublikong sasakyan ng pasahero; at
+(b) ang nagtatrabaho o taong namamahala sa isang pampublikong lugar o pampublikong sasakyan ng pasahero ay hindi dapat tumanggi sa pag-access sa lugar o sasakyan sa isang tao na buo o bahagyang bulag o bingi, o kung hindi man ay may kapansanan, sa lupa na ang tao ay sinamahan ng isang aso na may kapansanan, gabayan ang aso o aso na pandinig
+
+Pinakamataas na Fine para sa paglabag sa Batas:
+Nakatakdang parusa ng \$ 250
+
+Mga Pagbubukod sa Pag-access sa ilalim ng Batas:
+Hindi bawal sa tao ang isang hayop na may tulong na tatanungin upang makagawa ng katibayan na ang kanilang aso ay isang hayop na tumutulong.
+""",
+"""# Dog and Cat Management Act 1995
+## Section / s Anwendbar:
+§ 21, Akkreditierung von Behindertenhunden, Blindenhunden usw
+### Abschnitt 81, Behindertenhunde, Blindenhunde usw
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20und%20Cat%20Management%20Act%201995.aspx/)(http://www.legislation.sa.gov.au /LZ/C/A/Dog%20und%20Cat%20Management%20Act%201995.aspx/)
+
+Auszug aus § 81:
+(1) Trotz dieses Gesetzes oder eines anderen Gesetzes?
+(a) Eine Person, die ganz oder teilweise blind oder taub oder auf andere Weise behindert ist, hat Anspruch auf Begleitung eines anerkannten Hundes mit Behinderung, eines Blindenhundes oder eines hörenden Hundes in einem öffentlichen Ort oder einem öffentlichen Personenkraftwagen. und
+(b) Der Insasse oder die Person, die für einen öffentlichen Ort oder ein öffentliches Personenfahrzeug verantwortlich ist, darf einer Person, die ganz oder teilweise blind, taub oder auf andere Weise behindert ist, den Zugang zu dem Ort oder dem Fahrzeug nicht verweigern, weil die Person begleitet wird von einem behinderten Hund, einem Blindenhund oder einem hörenden Hund.
+
+Höchststrafe für Verstöße:
+Fixe Strafe von 250 \$
+
+Ausnahmen für den Zugang nach dem Gesetz:
+Es ist nicht rechtswidrig, wenn eine Person mit einem Hilfstier aufgefordert wird, den Nachweis zu erbringen, dass es sich bei ihrem Hund um ein Hilfstier handelt.
+""",
+"""# Νόμος περί διαχείρισης σκύλων και γάτων του 1995
+## Τμήμα / s Εφαρμοστέο:
+### Άρθρο 21, Διαπίστευση των Σκύλων Αναπηρίας, Σκύλοι Οδηγών κλπ
+### Τμήμα 81, Σκύλοι αναπηρίας, Σκύλοι οδηγοί κλπ
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+Απόσπασμα από το τμήμα 81:
+(1) Παρά την παρούσα Πράξη ή οποιαδήποτε άλλη Πράξη;
+α) ένα πρόσωπο που είναι εν όλω ή εν μέρει τυφλό ή κωφό ή έχει ανάπηρους με άλλο τρόπο, δικαιούται να συνοδεύεται από διαπιστευμένο σκύλο αναπηρίας, σκύλο οδηγό ή σκύλο ακρόασης σε δημόσιο χώρο ή δημόσιο επιβατικό όχημα · και
+β) ο κάτοχος ή ο υπεύθυνος δημόσιου χώρου ή δημόσιου επιβατικού οχήματος δεν πρέπει να αρνείται την πρόσβαση στον τόπο ή το όχημα σε πρόσωπο το οποίο είναι εν όλω ή εν μέρει τυφλό ή κωφό ή με άλλο τρόπο ανάπηρο, με το αιτιολογικό ότι το άτομο συνοδεύεται από σκύλο αναπηρίας, σκύλο οδηγού ή σκύλο ακοής.
+
+Μέγιστο πρόστιμο για παραβίαση νόμου:
+Σταθερή ποινή \$ 250
+
+Εξαιρέσεις στην πρόσβαση βάσει νόμου:
+Δεν είναι παράνομο να ζητηθεί από ένα άτομο με ζώο βοήθειας να αποδείξει ότι ο σκύλος του είναι ζώο βοήθειας.
+""",
+"""# डॉग एंड कैट मैनेजमेंट एक्ट 1995
+## धारा / एस लागू:
+### धारा 21, अक्षमता कुत्तों, गाइड कुत्तों आदि का प्रत्यायन
+### धारा 81, विकलांगता कुत्ते, गाइड कुत्ते आदि
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+धारा 81 से अंश:
+(१) इस अधिनियम या किसी अन्य अधिनियम के बावजूद?
+(ए) एक व्यक्ति जो पूरी तरह से या आंशिक रूप से अंधा या बहरा है, या अन्यथा अक्षम है, एक मान्यता प्राप्त विकलांगता कुत्ते, गाइड कुत्ते या एक सार्वजनिक स्थान या सार्वजनिक यात्री वाहन में कुत्ते की सुनवाई के साथ होने का हकदार है; तथा
+(बी) किसी सार्वजनिक स्थान या सार्वजनिक यात्री वाहन के प्रभारी या व्यक्ति को उस स्थान या वाहन तक पहुंच से इनकार नहीं करना चाहिए, जो पूरी तरह से या आंशिक रूप से अंधा या बहरा है, या अन्यथा विकलांग, इस आधार पर कि व्यक्ति साथ है एक विकलांगता कुत्ते, गाइड कुत्ते या सुनवाई कुत्ते द्वारा।
+
+अधिनियम के उल्लंघन के लिए अधिकतम जुर्माना:
+\$ 250 का जुर्माना
+
+अधिनियम के तहत प्रवेश के अपवाद:
+यह एक सहायता पशु के साथ एक व्यक्ति के लिए गैरकानूनी नहीं है, जिसे यह सबूत देने के लिए कहा जाए कि उनका कुत्ता एक सहायता जानवर है।
+""",
+"""# Dog and Cat Management Act 1995
+## Sezione / i applicabile:
+Sezione 21, Accreditamento di cani disabili, cani guida ecc
+Sezione 81, Cani per disabili, Cani guida ecc
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+Estratto dalla sezione 81:
+(1) Nonostante questa legge o qualsiasi altra legge?
+a) una persona che sia completamente o parzialmente cieca o sorda o diversamente disabilitata, ha il diritto di essere accompagnata da un cane disabile accreditato, un cane guida o un cane uditore in un luogo pubblico o un veicolo passeggeri pubblico; e
+(b) l'occupante o la persona responsabile di un luogo pubblico o di un veicolo passeggeri pubblico non deve rifiutare l'accesso al luogo o al veicolo a una persona che sia totalmente o parzialmente cieca o sorda, o diversamente disabilitata, per il motivo che la persona è accompagnata da un cane disabile, cane guida o cane uditore.
+
+Multa massima per violazione dell'atto:
+Penalità fissa di \$ 250
+
+Eccezioni all'accesso ai sensi della legge:
+Non è illegale che una persona con un animale di assistenza sia invitata a fornire prove del fatto che il suo cane è un animale di assistenza.
+""",
+"""# 개와 고양이 관리법 1995
+적용 가능한 섹션 :
+### 섹션 21, 장 애견, 안내견 등의 인증
+### 섹션 81, 장 애견, 안내견 등
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/](http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+섹션 81에서 발췌 :
+(1)이 법이나 다른 법에도 불구하고?
+(a) 전적으로 또는 부분적으로 맹인 또는 청각 장애가 있거나 장애인이 공공 장소 또는 공공 승용차에서 공인 장 애견, 안내견 또는 청각 견을 동반 할 자격이있는 사람; 과
+(b) 공공 장소 또는 공공 승용차를 책임지는 점유자 또는 사람은 그 사람이 동반 한 근거로 전적으로 또는 부분적으로 맹인 또는 청각 장애가 있거나 장애가있는 사람에 대한 장소 또는 차량에 대한 접근을 거부해서는 안됩니다. 장 애견, 안내견 또는 청각 견.
+
+법 위반에 대한 최대 벌금 :
+\$ 250의 고정 된 페널티
+
+법에 따라 액세스 할 수있는 예외 :
+보조 동물을 가진 # कुकुर र बिराला व्यवस्थापन ऐन १ 1995 1995
+## सेक्सन / हरू लागू:
+### धारा २१, अशक्तता कुकुर, गाईड कुकुर आदिका मान्यता
+### सेक्सन 81१, अक्षमता कुकुर, गाईड कुकुरहरू आदि
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/ চৌধুরী(http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+सेक्सन from१ बाट उद्धरण:
+(१) यो ऐन वा अरू कुनै ऐनको बाबजुद?
+(क) एक व्यक्ति जो पूर्ण वा आंशिक रूपमा अन्धा वा बहिरा, वा अन्यथा असक्षम छ, एक मान्यता प्राप्त अक्षमता कुकुर, गाईड कुकुर वा सार्वजनिक ठाउँमा वा सार्वजनिक यात्री गाडीमा सुन्न कुकुरको साथ जाने अधिकार छ; र
+(ख) सार्वजनिक स्थान वा सार्वजनिक यात्री सवारी साधनको कब्जामा लिने व्यक्ति वा व्यक्तिले पूर्ण रूपमा वा आंशिक रूपमा अन्धा वा बहिरा, वा अन्यथा असक्षम भएको व्यक्तिको लागि ठाउँ वा सवारी साधन पहुँच गर्न अस्वीकार गर्नु हुँदैन, व्यक्तिसँग भएको भूमिमा एक अशक्तता कुकुर, गाइड कुकुर वा सुन्ने कुकुर द्वारा।
+
+कानूनको उल्लंघनको लागि अधिकतम फाइनः
+Penalty २ of० को निश्चित पेनाल्टी
+
+ऐन अन्तर्गत पहुँच गर्न अपवाद:
+सहायता जनावर भएको व्यक्तिलाई उनीहरूको कुकुर सहयोगी जनावर हो भन्ने प्रमाण प्रस्तुत गर्न सोध्नु गैरकानूनी होईन।
+""",
+"""# قانون مدیریت سگ و گربه 1995
+## بخش / ها قابل اجرا:
+### بخش 21 ، اعتبار سنجی سگهای معلول ، سگهای راهنمای و غیره
+### بخش 81 ، سگهای معلولیت ، سگهای راهنمای و غیره
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog٪20and٪20Cat٪20Management٪20Act٪201995.aspx/LOAD(http://www.legislation.sa.gov.au /LZ/C/A/Dog٪20and٪20Cat٪20Management٪20Act٪201995.aspx/)
+
+گزیده ای از بخش 81:
+(1) با وجود این قانون یا هر قانون دیگری؟
+(الف) شخصی که کاملاً یا کاملاً نابینا یا ناشنوا است ، یا در غیر این صورت معلول است ، حق دارد با یک سگ معتبر معتبر ، سگ راهنما یا سگ شنوایی در یک مکان عمومی یا وسیله نقلیه عمومی مسافر همراه شود. و
+ب) اشغالگر یا شخص مسئول یک مکان عمومی یا وسیله نقلیه مسافر عمومی نباید از دسترسی به محل یا وسیله نقلیه به شخصی که کاملاً یا جزئی کاملاً نابینا یا ناشنوا است و یا در غیر این صورت از کار افتاده است ، امتناع کند به این دلیل که شخص همراه باشد. توسط سگ معلولیت ، سگ راهنما یا سگ شنوایی
+
+حداکثر جریمه برای نقض قانون:
+مجازات ثابت 250 دلار
+
+استثنائات دسترسی تحت قانون:
+غیرممکن نیست از کسی که یک حیوان کمک کننده است ، بخواهد شواهدی ارائه دهد که سگ آنها حیوان کمک باشد.
+""",
+"""# ਕੁੱਤਾ ਅਤੇ ਬਿੱਲੀ ਪ੍ਰਬੰਧਨ ਐਕਟ 1995
+## ਭਾਗ / ਸ ਲਾਗੂ
+### ਧਾਰਾ 21, ਅਪੰਗਤਾ ਕੁੱਤਿਆਂ ਦੀ ਪ੍ਰਾਪਤੀ, ਗਾਈਡ ਕੁੱਤੇ ਆਦਿ
+### ਧਾਰਾ 81, ਅਪੰਗਤਾ ਕੁੱਤੇ, ਗਾਈਡ ਕੁੱਤੇ ਆਦਿ
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/ চৌধুরী(http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+ਸੈਕਸ਼ਨ 81 81 ਤੋਂ ਅੰਸ਼:
+(1) ਇਸ ਐਕਟ ਜਾਂ ਕਿਸੇ ਹੋਰ ਐਕਟ ਦੇ ਬਾਵਜੂਦ?
+()) ਉਹ ਵਿਅਕਤੀ ਜੋ ਪੂਰੀ ਤਰ੍ਹਾਂ ਜਾਂ ਅੰਸ਼ਕ ਤੌਰ 'ਤੇ ਅੰਨ੍ਹਾ ਜਾਂ ਬੋਲ਼ਾ ਹੈ, ਜਾਂ ਹੋਰ ਅਪਾਹਜ ਹੈ, ਨੂੰ ਮਾਨਤਾ ਪ੍ਰਾਪਤ ਅਪੰਗਤਾ ਕੁੱਤਾ, ਗਾਈਡ ਕੁੱਤਾ ਜਾਂ ਸਰਵਜਨਕ ਜਗ੍ਹਾ ਜਾਂ ਜਨਤਕ ਯਾਤਰੀ ਵਾਹਨ ਵਿਚ ਸੁਣਨ ਵਾਲੇ ਕੁੱਤੇ ਦੇ ਨਾਲ ਜਾਣ ਦਾ ਹੱਕਦਾਰ ਹੈ; ਅਤੇ
+(ਅ) ਜਨਤਕ ਜਗ੍ਹਾ ਜਾਂ ਜਨਤਕ ਯਾਤਰੀ ਵਾਹਨ ਦਾ ਇੰਚਾਰਜ ਜਾਂ ਵਿਅਕਤੀ ਨੂੰ ਉਸ ਜਗ੍ਹਾ ਜਾਂ ਵਾਹਨ ਤਕ ਪਹੁੰਚਣ ਤੋਂ ਇਨਕਾਰ ਨਹੀਂ ਕਰਨਾ ਚਾਹੀਦਾ ਜਿਹੜਾ ਪੂਰੀ ਤਰ੍ਹਾਂ ਜਾਂ ਅੰਸ਼ਕ ਤੌਰ ਤੇ ਅੰਨ੍ਹਾ ਜਾਂ ਬੋਲ਼ਾ ਹੈ, ਜਾਂ ਹੋਰ ਅਪਾਹਜ ਹੈ, ਇਸ ਆਧਾਰ ਤੇ ਕਿ ਉਹ ਵਿਅਕਤੀ ਉਸ ਦੇ ਨਾਲ ਹੈ ਇੱਕ ਅਪੰਗਤਾ ਕੁੱਤਾ, ਗਾਈਡ ਕੁੱਤਾ ਜਾਂ ਸੁਣਨ ਵਾਲੇ ਕੁੱਤੇ ਦੁਆਰਾ.
+
+ਕਾਨੂੰਨ ਦੀ ਉਲੰਘਣਾ ਲਈ ਵੱਧ ਤੋਂ ਵੱਧ ਜੁਰਮਾਨਾ:
+Penalty 250 ਦਾ ਪੱਕਾ ਜ਼ੁਰਮਾਨਾ
+
+ਐਕਟ ਅਧੀਨ ਪਹੁੰਚ ਕਰਨ ਦੇ ਅਪਵਾਦ:
+ਸਹਾਇਤਾ ਪ੍ਰਾਪਤ ਜਾਨਵਰ ਵਾਲੇ ਵਿਅਕਤੀ ਲਈ ਇਹ ਗੈਰ-ਕਾਨੂੰਨੀ ਨਹੀਂ ਹੈ ਕਿ ਉਸ ਨੂੰ ਇਹ ਸਬੂਤ ਪੇਸ਼ ਕਰਨ ਲਈ ਕਿਹਾ ਜਾਵੇ ਕਿ ਉਨ੍ਹਾਂ ਦਾ ਕੁੱਤਾ ਸਹਾਇਤਾ ਜਾਨਵਰ ਹੈ.
+""",
+"""# Ley de gestión de perros y gatos de 1995
+## Sección / s aplicable:
+### Sección 21, Acreditación de perros con discapacidad, perros guía, etc.
+### Sección 81, Perros con discapacidad, perros guía, etc.
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/font>(http://www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Management%20Act%201995.aspx/)
+
+Extracto de la Sección 81:
+(1) ¿A pesar de esta Ley o de cualquier otra ley?
+(a) una persona que es total o parcialmente ciega o sorda, o de alguna otra forma discapacitada, tiene derecho a ser acompañada por un perro discapacitado acreditado, un perro guía o un perro oyente en un lugar público o en un vehículo público de pasajeros; y
+(b) el ocupante o la persona a cargo de un lugar público o vehículo público de pasajeros no debe negarle el acceso al lugar o vehículo a una persona que esté total o parcialmente ciega o sorda, o discapacitada, por el hecho de que la persona está acompañada por un perro discapacitado, un perro guía o un perro oyente.
+
+Multa máxima por incumplimiento de la ley:
+Penalidad fija de \$ 250
+
+Excepciones al acceso según la ley:
+No es ilegal pedirle a una persona con un animal de asistencia que presente pruebas de que su perro es un animal de asistencia.
+""",
+"""Đạo luật quản lý chó và mèo năm 1995
+## Phần / s Áp dụng:
+### Phần 21, Kiểm định chó khuyết tật, Chó dẫn đường, v.v.
+### Phần 81, Chó khuyết tật, Chó dẫn đường, v.v.
+[http://www.legislation.sa.gov.au/LZ/C/A/Dog%20and%20Cat%20Quản lý% 20Act% 201995.aspx / [(http: //www.legislation.sa.gov.au /LZ/C/A/Dog%20and%20Cat%20Quản lý% 20Act% 201995.aspx /)
+
+Trích từ Mục 81:
+(1) Bất chấp Đạo luật này hay Đạo luật nào khác?
+(a) một người bị mù hoàn toàn hoặc một phần hoặc điếc, hoặc khuyết tật, được quyền đi cùng với một con chó khuyết tật được công nhận, chó dẫn đường hoặc chó nghe ở nơi công cộng hoặc xe chở khách công cộng; và
+(b) người chiếm giữ hoặc người phụ trách nơi công cộng hoặc phương tiện chở khách công cộng không được từ chối tiếp cận địa điểm hoặc phương tiện với người bị mù hoàn toàn hoặc một phần hoặc điếc, hoặc bị khuyết tật, trên mặt đất mà người đó đi cùng bởi một con chó khuyết tật, chó dẫn đường hoặc chó nghe.
+
+Mức phạt tối đa cho Vi phạm Đạo luật:
+Đã sửa lỗi phạt \$ 250
+
+Các ngoại lệ đối với Access Under Act:
+Không phải là bất hợp pháp khi một người có động vật hỗ trợ được yêu cầu đưa ra bằng chứng rằng con chó của họ là động vật hỗ trợ.
+""",
+  ];
+}
