8671147c8991d1c0a320aa12b71044ae25a71292
Nathan <7258267@student.swin.edu.au>
2019-09-12 11:38:21 +1000

    wa translation

--- lib/data/markdown/wa.dart
+++ lib/data/markdown/wa.dart
@@ -1,4 +1,6 @@
-const String WA = """# Dog Act 1976
+class WA {
+  List<String> translation = [
+"""# Dog Act 1976
 ## Section/s Applicable:
 ### Section 8, Special Provisions for Guide Dogs
 [http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
@@ -7,5 +9,146 @@ Excerpt from Section 8:
 (1) Notwithstanding anything contained elsewhere in this Act or in any other Act, regulation, local law or by-law a person who is blind or partially blind?
 (a) is entitled to be accompanied by a dog bona fide used by him as a guide dog, in any building or place open to or used by the public, for any purpose, or in any public transport; and
 (b) is not guilty of an offence by reason only that he takes that dog into or permits that dog to enter any building or place open to or used by the public or on any public transport.
+""",
+"""# قانون الكلب 1976
+## القسم / ثانية قابلة للتطبيق:
+### القسم 8 ، أحكام خاصة للكلاب المرشدة
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
 
-""";
+مقتطف من القسم 8:
+(1) بصرف النظر عن أي شيء يرد في مكان آخر من هذا القانون أو في أي قانون آخر أو لائحة أو قانون محلي أو نظام داخلي ، أي شخص أعمى أو أعمى جزئيًا؟
+(أ) يحق له أن يرافقه كلب حسن النية يستخدمه ككلب مرشد ، في أي مبنى أو مكان مفتوح للجمهور أو يستخدمه ، لأي غرض ، أو في أي وسيلة نقل عامة ؛ و
+(ب) غير مذنب بارتكاب جريمة فقط لأنه يأخذ ذلك الكلب إلى ذلك الكلب أو يسمح له بالدخول إلى أي مبنى أو مكان مفتوح أو يستخدمه الجمهور أو في أي وسيلة نقل عامة.
+""",
+"""#Sog Act 1976
+##部分适用：
+###第8节，导盲犬特别规定
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/）
+
+摘自第8节：
+（1）尽管本法案或其他任何法案，法规，当地法律或法律中的任何内容都包含盲人或部分失明者？
+（a）在任何公众，任何目的或任何公共交通工具开放或使用的任何建筑物或地方，均有权使用他作为导盲犬使用的善意狗; 和
+（b）并非犯罪，只因为他将该狗带入或准许该狗进入任何对公众或任何公共交通工具开放或使用的建筑物或地方。
+""",
+"""#Sog Act 1976
+##部分適用：
+###第8節，導盲犬特別規定
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/）
+
+摘自第8節：
+（1）儘管本法案或其他任何法案，法規，當地法律或法律中的任何內容都包含盲人或部分失明者？
+（a）在任何公眾，任何目的或任何公共交通工具開放或使用的任何建築物或地方，均有權使用他作為導盲犬使用的善意狗; 和
+（b）並非犯罪，只因為他將該狗帶入或准許該狗進入任何對公眾或任何公共交通工具開放或使用的建築物或地方。
+""",
+"""# Batas ng Aso 1976
+# # Seksyon / s Naaangkop:
+### Seksyon 8, Mga Espesyal na Mga Provisyon para sa Mga Aso sa Gabay
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/etau(http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Sipi mula sa Seksyon 8:
+(1) Sa kabila ng anumang bagay na nakapaloob sa Batas na ito o sa anumang iba pang Batas, regulasyon, lokal na batas o by-law ng isang tao na bulag o bahagyang bulag?
+(a) ay karapat-dapat na samahan ng aso bona fide na ginamit niya bilang isang gabay na aso, sa anumang gusali o lugar na bukas o ginamit ng publiko, para sa anumang layunin, o sa anumang pampublikong transportasyon; at
+(b) ay hindi nagkasala ng isang pagkakasala sa kadahilanan lamang na isinasama niya o pinapayagan ang aso na pumasok sa anumang gusali o lugar na bukas o ginamit ng publiko o sa anumang pampublikong transportasyon.
+""",
+"""# Dog Act 1976
+## Section / s Anwendbar:
+### Abschnitt 8, Besondere Bestimmungen für Blindenhunde
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Auszug aus Abschnitt 8:
+(1) Ungeachtet aller in diesem Gesetz oder in anderen Gesetzen, Verordnungen, örtlichen Gesetzen oder Gesetzen enthaltenen Bestimmungen ist eine Person blind oder teilweise blind?
+a) berechtigt ist, von einem Hund begleitet zu werden, der von ihm als Blindenhund in einem Gebäude oder an einem Ort, der für die Öffentlichkeit zu irgendeinem Zweck oder in öffentlichen Verkehrsmitteln zugänglich ist, benutzt wird; und
+(b) nicht nur aus dem Grund einer Straftat schuldig ist, dass er diesen Hund in ein Gebäude oder einen Ort, der für die Öffentlichkeit oder in einem öffentlichen Transportmittel geöffnet oder von ihr benutzt wird, mitnimmt oder ihm gestattet.
+""",
+"""# Dog Act 1976
+## Τμήμα / s Εφαρμοστέο:
+### Τμήμα 8, Ειδικές διατάξεις για τα σκυλιά-οδηγούς
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Απόσπασμα από την Ενότητα 8:
+(1) Ανεξάρτητα από οτιδήποτε περιέχεται σε άλλο σημείο της παρούσας Πράξης ή σε οποιονδήποτε άλλο νόμο, κανονισμό, τοπικό νόμο ή νόμο, πρόσωπο που είναι τυφλός ή μερικώς τυφλός;
+α) δικαιούται να συνοδεύεται από καλόπιστο σκύλο που χρησιμοποιείται από αυτόν ως σκύλος-οδηγός, σε οποιοδήποτε κτίριο ή χώρο ανοικτό ή χρησιμοποιούμενο από το κοινό, για οποιονδήποτε σκοπό ή σε οποιαδήποτε δημόσια συγκοινωνία · και
+(β) δεν είναι ένοχος αδικήματος μόνο λόγω του ότι παίρνει το σκύλο αυτό ή επιτρέπει στο σκύλο αυτό να εισέλθει σε οποιοδήποτε κτίριο ή χώρο ανοικτό ή χρησιμοποιούμενο από το κοινό ή σε οποιαδήποτε δημόσια συγκοινωνία.
+""",
+"""# डॉग एक्ट 1976
+## धारा / एस लागू:
+### धारा 8, गाइड कुत्तों के लिए विशेष प्रावधान
+[Http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+धारा 8 से अंश:
+(१) इस अधिनियम में या किसी अन्य अधिनियम, विनियमन, स्थानीय कानून या उप-कानून में एक व्यक्ति जो कहीं अंधा या आंशिक रूप से अंधा है, के बावजूद कुछ भी शामिल नहीं है?
+(ए) एक कुत्ते के साथ उसके द्वारा इस्तेमाल किए जाने वाले सहयोगी के रूप में होने का हकदार है, जो किसी भी इमारत या जगह पर जनता के लिए खुला है या किसी भी उद्देश्य से, या किसी भी सार्वजनिक परिवहन में उपयोग किया जाता है; तथा
+(बी) केवल इस कारण से अपराध का दोषी नहीं है कि वह उस कुत्ते को अंदर ले जाए या उस कुत्ते को किसी भी इमारत या जगह में प्रवेश करने की अनुमति दे या जनता द्वारा या किसी सार्वजनिक परिवहन पर इस्तेमाल किया जाए।
+""",
+"""# Dog Act 1976
+## Sezione / i applicabile:
+### Sezione 8, Disposizioni speciali per i cani guida
+[Http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Estratto dalla sezione 8:
+(1) Nonostante quanto contenuto altrove nella presente legge o in qualsiasi altra legge, regolamento, legge locale o statuto di una persona che è cieca o parzialmente cieca?
+a) ha il diritto di essere accompagnato da un cane in buona fede da lui utilizzato come cane guida, in qualsiasi edificio o luogo aperto o utilizzato dal pubblico, per qualsiasi scopo o in qualsiasi trasporto pubblico; e
+(b) non è colpevole di un reato solo perché porta quel cane o consente a quel cane di entrare in qualsiasi edificio o luogo aperto o utilizzato dal pubblico o su qualsiasi trasporto pubblico.
+""",
+"""# 개법 1976
+적용 가능한 섹션 :
+### 섹션 8, 안내견 특별 조항
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/](http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+섹션 8에서 발췌 :
+(1)이 법의 다른 곳이나 다른 법, 규정, 현지 법 또는 조례에 포함 된 내용에도 불구하고 맹인 또는 부분 맹인 인 사람?
+가. 안내견으로 사용되는 개 선의를 동반 할 권리가 있으며, 대중이 개방하거나 사용하는 건물이나 장소, 대중의 목적 또는 대중 교통 수단 과
+(b) 그 개가 그 개를 대중에게 또는 공공 교통 수단에 개방 또는 사용되는 건물이나 장소에 출입 시키거나 허용하는 이유만으로 범죄에 대해 유죄가 아니다.
+""",
+"""# कुकुर ऐन १ 6 66
+## सेक्सन / हरू लागू:
+### सेक्सन 8, गाइड कुकुरहरूका लागि विशेष प्रावधान
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/ छुट(http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+सेक्शन from बाट उद्धरण:
+(१) यस ऐन वा अन्य कुनै ऐन, नियम, स्थानीय कानून वा उप-कानूनमा अन्ततः अन्ततः अन्धा वा अन्धो भएको व्यक्तिलाई समावेश गरे पनि?
+(क) कुनै गाँउ कुकुरको रूपमा उनीद्वारा प्रयोग गरिएको कुकुर बोना फिडको साथमा, कुनै भवन वा स्थानमा सार्वजनिक रूपमा खुला वा सार्वजनिक रूपमा प्रयोग गरिएको, कुनै उद्देश्यका लागि, वा कुनै सार्वजनिक यातायातमा हकदार छ; र
+(बी) केवल कारणको लागि कुनै अपराधको लागि दोषी छैन कि उसले त्यो कुकुरलाई भित्र राख्छ वा त्यो कुकुरलाई कुनै भवन वा सार्वजनिक ठाउँमा वा सार्वजनिक वा कुनै सार्वजनिक यातायातमा प्रयोग गरिएको ठाउँमा प्रवेश गर्न अनुमति दिन्छ।
+""",
+"""# قانون سگ 1976
+## بخش / ها قابل اجرا:
+### بخش 8 ، مقررات ویژه سگهای راهنما
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/((http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+گزیده ای از بخش 8:
+(1) با وجود هر چیز دیگری در این قانون یا هر قانون ، آیین نامه ، قوانین محلی یا آیین نامه شخصی که کور یا جزئی کور است ، وجود دارد؟
+(الف) حق دارد با یك سگی كه به عنوان یك سگ راهنما از او استفاده می شود ، در هر ساختمان یا مكانی كه برای عموم یا برای هر منظور و یا برای هر وسیله حمل و نقل عمومی مورد استفاده قرار گیرد ، همراه باشد. و
+ب) فقط به این دلیل که مقصر این سگ نیست ، مقصر است و آن سگ را وارد می کند یا به آن اجازه می دهد که وارد هر ساختمان یا مکانی شود که توسط عموم یا هر وسیله حمل و نقل عمومی مورد استفاده قرار گیرد.
+""",
+"""# ਕੁੱਤਾ ਐਕਟ 1976
+## ਭਾਗ / ਸ ਲਾਗੂ
+### ਸੈਕਸ਼ਨ 8, ਗਾਈਡ ਕੁੱਤਿਆਂ ਲਈ ਵਿਸ਼ੇਸ਼ ਪ੍ਰਬੰਧ
+]
+
+ਸੈਕਸ਼ਨ 8 ਤੋਂ ਅੰਸ਼:
+(1) ਇਸ ਐਕਟ ਵਿਚ ਜਾਂ ਕਿਸੇ ਹੋਰ ਐਕਟ, ਨਿਯਮ, ਸਥਾਨਕ ਕਾਨੂੰਨ ਜਾਂ ਬਾਈ-ਲਾਅ ਵਿਚ ਕਿਤੇ ਵੀ ਪਾਈ ਗਈ ਕਿਸੇ ਵੀ ਚੀਜ਼ ਦੇ ਬਾਵਜੂਦ, ਅੰਨ੍ਹਾ ਜਾਂ ਅੰਸ਼ਕ ਤੌਰ ਤੇ ਅੰਨ੍ਹਾ ਹੈ?
+(ਏ) ਕਿਸੇ ਗੈਰ-ਕਾਨੂੰਨੀ ਜਾਂ ਕਿਸੇ ਜਨਤਕ ਟ੍ਰਾਂਸਪੋਰਟ ਵਿਚ, ਕਿਸੇ ਇਮਾਰਤ ਜਾਂ ਜਗ੍ਹਾ ਵਿਚ, ਜਨਤਾ ਦੁਆਰਾ ਖੁੱਲ੍ਹੀ ਜਾਂ ਜਨਤਕ ਦੁਆਰਾ ਵਰਤੀ ਜਾਂਦੀ, ਕਿਸੇ ਗਾਈਡ ਕੁੱਤੇ ਦੇ ਤੌਰ ਤੇ ਉਸ ਦੁਆਰਾ ਵਰਤੇ ਜਾਂਦੇ ਕੁੱਤੇ ਦੀ ਬੇਰਹਿਮੀ ਨਾਲ ਆਉਣ ਦਾ ਹੱਕਦਾਰ ਹੈ; ਅਤੇ
+(ਬੀ) ਕਿਸੇ ਅਪਰਾਧ ਲਈ ਸਿਰਫ ਕਾਰਨ ਕਰਕੇ ਦੋਸ਼ੀ ਨਹੀਂ ਹੈ ਕਿ ਉਹ ਉਸ ਕੁੱਤੇ ਨੂੰ ਅੰਦਰ ਲੈ ਜਾਂਦਾ ਹੈ ਜਾਂ ਉਸ ਕੁੱਤੇ ਨੂੰ ਕਿਸੇ ਇਮਾਰਤ ਜਾਂ ਜਗ੍ਹਾ ਵਿਚ ਦਾਖਲ ਹੋਣ ਲਈ ਜਾਂ ਜਨਤਾ ਦੁਆਰਾ ਖੋਲ੍ਹਿਆ ਜਾਂ ਕਿਸੇ ਜਨਤਕ ਆਵਾਜਾਈ 'ਤੇ ਆਗਿਆ ਦਿੰਦਾ ਹੈ.
+""",
+"""# Dog Act 1976
+## Sección / s aplicable:
+### Sección 8, Disposiciones especiales para perros guía
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/font>(http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Extracto de la Sección 8:
+(1) A pesar de cualquier cosa contenida en otra parte de esta Ley o en cualquier otra Ley, reglamento, ley local o estatuto, ¿una persona ciega o parcialmente ciega?
+(a) tiene derecho a estar acompañado por un perro de buena fe utilizado por él como perro guía, en cualquier edificio o lugar abierto o utilizado por el público, para cualquier propósito o en cualquier transporte público; y
+(b) no es culpable de un delito por el solo hecho de que ingresa a ese perro o permite que el perro ingrese a cualquier edificio o lugar abierto o utilizado por el público o en cualquier transporte público.
+""",
+"""Đạo luật # Dog 1976
+## Phần / s Áp dụng:
+### Phần 8, Quy định đặc biệt dành cho chó dẫn đường
+[http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/[(http://www.austlii.edu.au/au/legis/wa/consol_act/da197644/)
+
+Trích từ Phần 8:
+(1) Mặc dù có bất cứ điều gì có trong bất kỳ nơi nào khác trong Đạo luật này hoặc trong bất kỳ Đạo luật, quy định, luật địa phương hoặc luật pháp khác, một người bị mù hoặc mù một phần?
+(a) có quyền đi cùng với một chú chó bona fide được anh ta sử dụng làm chó dẫn đường, trong bất kỳ tòa nhà hoặc nơi nào được mở hoặc sử dụng cho công chúng, cho bất kỳ mục đích nào, hoặc trong bất kỳ phương tiện giao thông công cộng nào; và
+(b) không phạm tội chỉ vì lý do anh ta đưa con chó đó vào hoặc cho phép con chó đó vào bất kỳ tòa nhà nào hoặc nơi công cộng mở hoặc sử dụng trên bất kỳ phương tiện giao thông công cộng nào.
+""",
+  ];
+}
