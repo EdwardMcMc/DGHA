0c505fe75b41e4e7e927fd84eebc664841f5e57b
jackmack99 <jmackey996@gmail.com>
2019-09-12 11:06:19 +1000

    act translation

--- lib/screens/info/legislation/translate_popup.dart
+++ lib/screens/info/legislation/translate_popup.dart
@@ -8,7 +8,7 @@ class TranslatePopup extends StatefulWidget {
 }
 
 class _TranslatePopup extends State<TranslatePopup> {
-    List<String> _languages = ['English', 'Arabic', 'Chinese (Simplified)', 'Chinese (Traditional)', 'German','Greek', 'Hindi', 'Italian', 'Korean',  'Nepali', 'Persian', 'Punjabi', 'Spanish','Filipino' ,'Vietnamese' ];
+    List<String> _languages = ['English', 'Arabic', 'Chinese (Simplified)', 'Chinese (Traditional)','Filipino', 'German','Greek', 'Hindi', 'Italian', 'Korean',  'Nepali', 'Persian', 'Punjabi', 'Spanish' ,'Vietnamese' ];
     String _selectedLanguage;
 
     @override
