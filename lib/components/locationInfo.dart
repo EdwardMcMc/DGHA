8aa37308a8846879982f0d02e0d9b811c8a8ac55
Nathan <7258267@student.swin.edu.au>
2019-10-16 09:53:29 +1100

    rating widget

--- lib/components/locationInfo.dart
+++ lib/components/locationInfo.dart
@@ -3,21 +3,48 @@ import 'package:dgha/style.dart';
 
 class Locationinfo extends StatelessWidget {
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Location Info"),),
-      body: rating(),
+      appBar: AppBar(
+        title: Text("Location Info"),
+      ),
+      body: Container(
+          child: Row(
+        children: <Widget>[
+          ratings(),
+          ratings(),
+          ratings(),
+          ratings(),
+        ],
+      )),
     );
   }
 
-  Widget rating() {
+  Widget ratings() {
     return Container(
-      child: Row(
-        children: <Widget>[
-          Text("* Rating"),
-          Text("Overall"),          
-        ],
-      ),
+      child: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: 65.0,
+            ),
+            Text("4.5",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                )),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text("Overall",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                ))
+          ],
+        )
+      ]),
     );
   }
 }
