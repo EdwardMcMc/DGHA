7c86b3f426703a7dc255a29b581edece90e132f1
jackmack99 <jmackey996@gmail.com>
2019-10-23 14:33:46 +1100

    review form UI, gitignore, min flutter ver
    review form now has star system and other thoughts textbox
    updated gitignore, primarily to get rid of issues with flutter_export_environment.sh
    changed minimum flutter version from 2.1.0 to 2.2.2, so that some shit I
    needed to do with for loops works I guess

--- lib/screens/review/review_form.dart
+++ lib/screens/review/review_form.dart
@@ -1,94 +1,151 @@
 import 'package:flutter/material.dart';
 
 class ReviewForm extends StatefulWidget {
-  const ReviewForm({ Key key }) : super(key: key);
 
   @override
   _ReviewForm createState() => _ReviewForm();
 }
 
 class _ReviewForm extends State<ReviewForm> {
-  bool isPressed = false;
-  int numPressed = 0;
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(title: Text('Review')),
-     body: Column(
-       children: [
-        Row(children: [
-          IconButton(
-            icon:
-              (numPressed == 0) ? Icon(Icons.star_border) : Icon(Icons.star),
-            color: 
-              (numPressed == 0) ? Colors.grey : Colors.yellow,
-            iconSize: 50,
-            onPressed: () {
-              
-              setState(() {
-                numPressed = 1;                
-              });
-            },
-          ),
-           IconButton(
-            icon:
-              (numPressed <= 1) ? Icon(Icons.star_border) : Icon(Icons.star),
-            color: 
-              (numPressed <= 1) ? Colors.grey : Colors.yellow,
-            iconSize: 50,
-            onPressed: () {
-              setState(() {
-                numPressed = 2;                
-              });
-            },
-          ),
-            IconButton(
-            icon:
-              (numPressed <= 2) ? Icon(Icons.star_border) : Icon(Icons.star),
-            color: 
-              (numPressed <= 2) ? Colors.grey : Colors.yellow,
-            iconSize: 50,
-            onPressed: () {
-              setState(() {
-                numPressed = 3;                
-              });
-            },
-          ),
-            IconButton(
-            icon:
-              (numPressed <= 3) ? Icon(Icons.star_border) : Icon(Icons.star),
-            color: 
-              (numPressed <= 3) ? Colors.grey : Colors.yellow,
-            iconSize: 50,
-            onPressed: () {
-              setState(() {
-                numPressed = 4;                
-              });
-            },
-          ),
-             IconButton(
-            icon:
-              (numPressed <= 4) ? Icon(Icons.star_border) : Icon(Icons.star),
-            color: 
-              (numPressed <= 4) ? Colors.grey : Colors.yellow,
-            iconSize: 50,
-            onPressed: () {
-              setState(() {
-                numPressed = 5;                
-              });
-            },
-          ),
-          ]
-        ),
-         
-        
+int locRating = 0;
+int amenRating = 0;
+int custRating = 0;
+bool didTrySubmit = false;
+final _formKey = GlobalKey<FormState>();
 
-       ],
-     )
-     
-     
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('Review',)),
+    body: SingleChildScrollView(
+      child: Form(
+      key: _formKey,
+      child:Column(
+        children: [
+            Text(
+               "Location",
+               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+            ),
+          
+          
+          Row(children: [
+            
+            for(int i=1; i<=5; i++)
+              IconButton(
+                icon:
+                  (locRating < i) ? Icon(Icons.star_border) : Icon(Icons.star),
+                color: 
+                  (locRating < i) ? Colors.grey : Colors.yellow,
+                iconSize: 50,
+                tooltip: "Location $i stars",
+                onPressed: () {
+                  setState(() {
+                    locRating = i;                           
+                  });
+                },
+              ),
+              ]
+            ),
+          if(didTrySubmit && locRating == 0)
+            Align(
+              alignment: Alignment.centerLeft,
+              child:Text(
+              "Please select a star rating",
+              style: TextStyle(color: Colors.red, fontSize: 12),
+              ),
+            ),
+          Text(
+               "Amenities",
+               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+            ),
+          Row(children: [
+            for(int i=1; i<=5; i++)
+              IconButton(
+                icon:
+                  (amenRating < i) ? Icon(Icons.star_border) : Icon(Icons.star),
+                color: 
+                  (amenRating < i) ? Colors.grey : Colors.yellow,
+                iconSize: 50,
+                tooltip: "Amenities $i stars",
+                onPressed: () {
+                  setState(() {
+                    amenRating = i;
+                  });
+                },
+              ),
+            ],
+          ),
+          if(didTrySubmit && amenRating == 0)
+          Align(
+            alignment: Alignment.centerLeft,
+            child:Text(
+            "Please select a star rating",
+            style: TextStyle(color: Colors.red, fontSize: 12),
+            ),
+          ),
+          Text(
+               "Customer Service",
+               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+            ),
+          
+          Row(children: [
+            for(int i=1; i<=5; i++)
+              IconButton(
+                icon:
+                  (custRating < i) ? Icon(Icons.star_border) : Icon(Icons.star),
+                color: 
+                  (custRating < i) ? Colors.grey : Colors.yellow,
+                iconSize: 50,
+                tooltip: "Customer Service $i stars",
+                onPressed: () {
+                  setState(() {
+                    custRating = i;
+                  });
+                },
+              ),
+            ],
+          ),
+          if(didTrySubmit && custRating == 0)
+          Align(
+            alignment: Alignment.centerLeft,
+            child:Text(
+            "Please select a star rating",
+            style: TextStyle(color: Colors.red, fontSize: 12),
+            ),
+          ),
+          Text(
+              "Other Thoughts",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+          ),
+          TextFormField(
+            validator: (value){
+              if (value.isEmpty){
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: "dart is good"
+            ),
+          ),
+          RaisedButton(
+            onPressed: (){
+              setState(() {
+                didTrySubmit = true;
+              });
+
+            if (_formKey.currentState.validate()&& locRating != 0 && amenRating != 0 && custRating !=0){
+              Scaffold
+                .of(context)
+                .showSnackBar (SnackBar(content: Text('Processing Data'),));
+              }
+            },
+            child: Text('Submit'),
+          )
+        ],
+      )
+    ),
+    )
    );
  }
-
- 
 }
